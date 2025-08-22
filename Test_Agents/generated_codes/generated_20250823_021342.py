e3c81650503bcceb1db843de5c93fd83ccb9ef31
m-ramuk-nura <marunkumar5767@gmail.com>
2025-08-23 02:13:42 +0530

    Add generated code 20250823_021342

---
+++ Test_Agents/generated_codes/generated_20250823_021342.py
@@ -0,0 +1,14 @@
+try:
+    # Define given strings
+    s1 = 'ab'
+    s2 = 'bddwasabsw'
+
+    # Check if s1 is a substring of s2
+    result = s1 in s2
+
+    # Output the result
+    print(result)
+except Exception as e:
+    import traceback
+    print('Error:', e)
+    print(traceback.format_exc())
