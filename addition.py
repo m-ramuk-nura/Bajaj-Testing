ceeb2479b6d3d72c7dba8355ab2497bb653f4aa6
m-ramuk-nura <marunkumar5767@gmail.com>
2025-08-23 01:01:20 +0530

    Add addition.py script

---
+++ addition.py
@@ -0,0 +1,11 @@
+# addition.py
+
+# Take two numbers as input
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Perform addition
+result = num1 + num2
+
+# Print the result
+print("The sum is:", result)
